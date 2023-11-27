dbaa26be9c4be7a2ebeae6ca7ebd524fee524c33
Poussy <112729185+PoussyAyman@users.noreply.github.com>
2023-11-27 16:43:02 +0200

    Update datafood_webapp.sql

--- datafood_webapp.sql
+++ datafood_webapp.sql
@@ -1,4 +1,4 @@
-USE food_webapp;
+USE datafood_webapp;
 
 CREATE TABLE customer (
     customer_id INT PRIMARY KEY
@@ -11,28 +11,25 @@ CREATE TABLE users (
 );
 CREATE TABLE Administrator (
     Admin_ID INT PRIMARY KEY,
-    Admin_name VARCHAR,
-    Email CHAR(40),
+    Admin_name VARCHAR(20),
+    Email varchar(50),
     phone_number INT,
     Password CHAR(10),
     user_ID INT,  
     FOREIGN KEY (user_ID) REFERENCES users(ID)
 );
-DROP TABLE IF EXISTS  Menu ;
 
 CREATE TABLE Menu(
 	Name VARCHAR,
 	Price INT,
-	Category char(20),
+	Category varchar(20),
 	 Admin_ID INT,  
     FOREIGN KEY (Admin_ID) REFERENCES Administrator(Admin_ID)
 );
-DROP TABLE IF EXISTS Promotion ;
-
 CREATE TABLE Payment(
 	code INT,
-	Coupon_ID INT Primary key, -- is it primary key?
-	Type Char(20),
+	Coupon_ID INT Primary key,
+	Type varChar(20),
 	customer_id INT,
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
@@ -45,14 +42,14 @@ CREATE TABLE The_Order (
     Order_ID INT Primary KEY,	
     customer_id INT,
     FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    Status varchar,
+    Status varchar(50),
     Total_Price DECIMAL(10, 2),
     OrderDateTime DATETIME
 );
 
 CREATE TABLE Promotion (
-    Coupon_ID INT Primary key,  -- is it P_KEY?
-    Code VARCHAR,	 
+    Coupon_ID INT Primary key,  
+    Code VARCHAR(10),	 
     Discount_Percentage FLOAT,
     Order_ID INT,
     FOREIGN KEY (Order_ID) REFERENCES The_Order(Order_ID)
@@ -61,7 +58,7 @@ CREATE TABLE Promotion (
 CREATE TABLE Review (
  Review_id int primary key,
  rate int,
- comment varchar,
+ comment varchar(200),
 
 );
 
@@ -87,7 +84,6 @@ CREATE TABLE Has_order(
 order_itemsID INT, FOREIGN KEY (order_itemsID) REFERENCES Order_items(order_itemsID),
 order_id INT, FOREIGN KEY (order_id) REFERENCES The_Order(order_id),
 );
-DROP TABLE IF EXISTS order_items  ;
 
 CREATE TABLE order_items(
     order_item_ID int Primary key,
@@ -106,21 +102,12 @@ FOREIGN KEY (customization_id) REFERENCES order_has_cust(customization_id),
 
 customization_id INT primary KEY,
 );
-DROP TABLE IF EXISTS  customizations ,order_has_cust;
-
+DROP TABLE IF EXISTS customizations ;
 CREATE TABLE customizations (
 	customization_id INT primary KEY,
 	price INT  ,
-	avalilibility char ,
+	avalilibility varchar(10) ,
 	description char,
-);
-DROP TABLE IF EXISTS   has_order ;
-CREATE TABLE  has_order(
-       order_item_ID  int,
-       FOREIGN KEY (order_item_ID) REFERENCES order_items(order_item_ID),
-	   order_ID int , 
-	   FOREIGN KEY (order_ID) REFERENCES the_order(order_ID),
-
 );
 
 
