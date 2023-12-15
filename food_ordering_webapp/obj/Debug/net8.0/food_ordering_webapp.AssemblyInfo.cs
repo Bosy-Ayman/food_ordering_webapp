19411969c1d0fa470652d645bca3f9af074e00e8
Poussy <poussy.ayman1@gmail.com>
2023-12-15 17:00:57 +0200

    create asp

--- food_ordering_webapp/obj/Debug/net8.0/food_ordering_webapp.AssemblyInfo.cs
+++ food_ordering_webapp/obj/Debug/net8.0/food_ordering_webapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("food_ordering_webapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30aa9f7d89e0b28dff903788a4714382ff349e72")]
 [assembly: System.Reflection.AssemblyProductAttribute("food_ordering_webapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("food_ordering_webapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
