6bc3bca2688330e7a42458d00a9ce19495d6e6d4
Poussy <poussy.ayman1@gmail.com>
2023-12-16 09:43:36 +0200

    create

---
+++ food_webapp_projectt/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace food_webapp_projectt.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
