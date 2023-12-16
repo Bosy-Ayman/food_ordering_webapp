33dbe7822a643a93cb750f829c9c5ba949c18cc3
Poussy <poussy.ayman1@gmail.com>
2023-12-16 16:04:30 +0200

    edit

--- food_webapp_projectt/obj/Debug/net8.0/food_webapp_projectt.AssemblyInfo.cs
+++ food_webapp_projectt/obj/Debug/net8.0/food_webapp_projectt.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("food_webapp_projectt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0db093ec26831e6f6f3ab080194a06fe016f35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("food_webapp_projectt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("food_webapp_projectt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ projectt/Pages/payment.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace projectt.Pages
+{
+    public class paymentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
