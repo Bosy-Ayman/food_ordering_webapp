d0434f6dff6486b10012541982e4699e403ab8dc
Poussy <poussy.ayman1@gmail.com>
2023-12-27 19:29:40 +0200

    new project folder

--- food_webapp_projectt/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace food_webapp_projectt.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
---
+++ old/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+using food_webapp_projectt.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace food_webapp_projectt.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+       
+     
+        public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+		public IActionResult login()
+		{
+			return View();
+		}
+		public IActionResult cart()
+		{
+			return View();
+		}
+        public IActionResult dashboard()
+        {
+            return View();
+        }
+        public IActionResult user_profile()
+        {
+            return View();
+        }
+
+        public IActionResult payment()
+        {
+            return View();
+        }
+        public IActionResult item_single()
+        {
+            return View();
+        }
+        public IActionResult items_single()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+
+    
+}
+
+}
+
+
+namespace food_webapp_projectt.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+   
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            if (username == "poussy.ayman1@gmail.com" && password == "2023")
+            {
+                // Redirect to dashboard
+                return RedirectToAction("Dashboard", "Home");
+
+            }else if(username == "user" && password == "user") //it will added be from the database later
+            {
+
+                return RedirectToAction("user_profile", "Home");
+            }
+            else
+            {
+              
+                return RedirectToAction("Login", "Home");
+            }
+        }
+        public ActionResult user_profile()
+        {
+            return View();
+        }
+
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+       
+    }
+}
