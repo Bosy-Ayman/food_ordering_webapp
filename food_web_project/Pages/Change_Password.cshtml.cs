509db22cbed01fff5b7da0b37217940e10e86fbe
Poussy <poussy.ayman1@gmail.com>
2024-01-02 02:05:48 +0200

    Synchronise pages

--- food_web_project/Pages/Change_Password.cshtml.cs
+++ food_web_project/Pages/Change_Password.cshtml.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace food_web_project.Pages
 {
-    public class Reviews_AdminModel : PageModel
+    public class Change_PasswordModel : PageModel
     {
         public void OnGet()
         {
