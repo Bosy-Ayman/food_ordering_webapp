0417a72d8c9e89cf20db5705d68924aae243f568
Poussy <poussy.ayman1@gmail.com>
2024-01-02 18:25:39 +0200

    add pages

--- food_web_project/Pages/SignUp.cshtml.cs
+++ food_web_project/Pages/SignUp.cshtml.cs
@@ -1,3 +1,4 @@
+using food_web_project.Models.Tables;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,17 +7,12 @@ namespace food_web_project.Pages
     public class SignUpModel : PageModel
     {
         [BindProperty]
+        public Customer customer { get; set; }
 
-        public string Username { get; set; } = string.Empty;
 
-        [BindProperty]
-        public string Password { get; set; } = string.Empty;
-        [BindProperty]
-        public string Phone_Number { get; set; } = string.Empty;
-        [BindProperty]
-        public string Email { get; set; } = string.Empty;
         public void OnGet()
         {
+
         }
        
     }
--- food_web_project/Pages/Sign_In.cshtml.cs
+++ food_web_project/Pages/Sign_In.cshtml.cs
@@ -5,30 +5,68 @@ namespace food_web_project.Pages
 {
     public class Sign_UpModel : PageModel
     {
-		[BindProperty]
-		public string Username { get; set; } = string.Empty;
+        [BindProperty]
+        public string Username { get; set; } = string.Empty;
 
-		[BindProperty]
-		public string Password { get; set; } = string.Empty;
-		public void OnGet()
+        [BindProperty]
+        public string Password { get; set; } = string.Empty;
+        public void OnGet()
         {
         }
 
-		public IActionResult OnPost()
-		{
-			if (Username == "me@gmail.com" && Password == "2023")//admin
-			{
-				return RedirectToPage("/User_Profile");
-			}
-			else if (Username == "user2@gmail.com" && Password == "2023")//user from the database
-			{
-				return RedirectToPage("/Dashboard");
-			}
-			else
-			{
-				ViewData["ErrorMessage"] = "Invalid username or password";
-				return Page();
-			}
-		}
-	}
+        public IActionResult OnPost()
+        {
+            if (Username == "me@gmail.com" && Password == "2023")//admin
+            {
+                return RedirectToPage("/User_Profile");
+            }
+            else if (Username == "user2@gmail.com" && Password == "2023")//user from the database
+            {
+                return RedirectToPage("/Dashboard");
+            }
+            else
+            {
+                ViewData["ErrorMessage"] = "Invalid username or password";
+                return Page();
+            }
+        }
+    }
 }
+
+
+/*using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using food_web_project.Models;
+using food_web_project.Models.Tables;
+namespace food_web_project.Pages
+{
+    public class Sign_UpModel : PageModel
+    {
+        [BindProperty]
+        public Users Users { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+       *//* public IActionResult OnPost()
+        {
+            DB db = new DB();
+            Users authenticatedUser = db.AuthenticateUser(Users.Username, Users.Password);
+
+            if (authenticatedUser != null)
+            {
+                // Redirect to the user's profile page based on their ID
+                return RedirectToPage("/UserProfile", new { userId = authenticatedUser.UserId });
+            }
+            else
+            {
+                ViewData["ErrorMessage"] = "Invalid username or password";
+                return Page();
+            }
+        }*//*
+    }
+
+
+}
+*/
